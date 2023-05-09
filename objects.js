1c9f885ae0fc7a8c6cfaa5e77051248e8b0a4334
Benjamin Cook <benjamin.cook@devmounta.in>
2023-05-09 17:39:14 -0600

    first commit

---
+++ objects.js
@@ -0,0 +1,38 @@
+
+let person = {
+    name: "Sean",
+    age: 23,
+    favorites: {
+        food: "steak",
+        game: "Zelda",
+        color: "Red",
+        number: 13
+    },
+    shoppingList: ["Banans", "Apples", "TP", "Milk", "cheese"]
+};
+
+// how to access values 
+
+// destructuring
+const { name, age } = person;
+// dot notation
+person.name;
+person.age;
+//bracket notation
+person["age"];
+person["name"];
+
+for(let key in person){
+    console.log(key, person[key]);
+}
+
+// add key value pair to the object 
+person.isMarried = true;
+person.isMarried = false;
+
+// remove key value pair
+delete person.age;
+
+
+console.log(person);
+// console.log(person.shoppingList[3]);
